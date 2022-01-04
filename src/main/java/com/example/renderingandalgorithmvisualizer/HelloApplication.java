865f182b1f0381d2e9148fbff1ab3eb97ad5c1ee
Garrett Harty <87092943+gharty03@users.noreply.github.com>
2022-01-03 22:27:25 -0800

    Here is initial project setup

--- src/main/java/com/example/renderingandalgorithmvisualizer/HelloApplication.java
+++ src/main/java/com/example/renderingandalgorithmvisualizer/HelloApplication.java
@@ -2,22 +2,28 @@ package com.example.renderingandalgorithmvisualizer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MenuWindow.fxml")));
+        Scene scene = new Scene(root);
+        stage.setTitle("Menu");
         stage.setScene(scene);
+        stage.setWidth(1080);
+        stage.setHeight(1080);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
