7d7e419dea6114029f0f7770367356e4b074eea4
Cody Machine <codymachine93@gmail.com>
2022-01-07 13:29:53 -0800

    Add Slider to Control Animation Timer Speed

--- src/main/java/com/example/renderingandalgorithmvisualizer/GameOfLifeController.java
+++ src/main/java/com/example/renderingandalgorithmvisualizer/GameOfLifeController.java
@@ -1,6 +1,8 @@
 package com.example.renderingandalgorithmvisualizer;
 
 import javafx.animation.AnimationTimer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -26,22 +29,23 @@ public class GameOfLifeController
     private Canvas canvas;
     @FXML
     private Button startButton, pauseButton, stepButton, stopButton, backButton, resumeButton;
-    private AnimationTimer gt;
-
     @FXML
-    protected void openMenuWindow(ActionEvent event) throws IOException
+    private Slider speedSlider;
+    private AnimationTimer gt;
+    private long timerSpeed;
+    private long timerBaseSpeed = 5000000L;
+
+    protected void initGameOfLifeWindow()
     {
-        fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MenuWindow.fxml"));
-        root = fxmlLoader.load();
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setWidth(450);
-        stage.setHeight(400);
-        stage.setTitle("RAAV - Rendering and Algorithm Visualizer");
-        stage.centerOnScreen();
-        stage.show();
-        if (gt != null) {gt.stop();}
+        //todo
+        speedSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldNum, Number newNum) {
+                timerSpeed = (int)Math.pow(2, (10 - newNum.intValue())) * timerBaseSpeed;
+            }
+        });
+
+
     }
 
     @FXML
@@ -90,17 +94,44 @@ public class GameOfLifeController
         // todo
     }
 
+    protected void onSpeedSliderValueChanged(ActionEvent event)
+    {
+        //todo
+    }
+
+    @FXML
+    protected void openMenuWindow(ActionEvent event) throws IOException
+    {
+        fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MenuWindow.fxml"));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setWidth(450);
+        stage.setHeight(400);
+        stage.setTitle("RAAV - Rendering and Algorithm Visualizer");
+        stage.centerOnScreen();
+        stage.show();
+        if (gt != null) {gt.stop();}
+    }
+
     private class GOLTimer extends AnimationTimer
     {
         private double curX,curY;
+        private long lastUpdatedTime;
 
         @Override
-        public void handle(long l)
+        public void handle(long currentTime)
         {
-            GraphicsContext gc = canvas.getGraphicsContext2D();
-            gc.clearRect(0,0,1000,1000);
-            gc.setFill(Color.AQUA);
-            gc.fillOval(curX += 1, curY,40,40);
+            if (currentTime - lastUpdatedTime >= timerSpeed)
+            {
+                lastUpdatedTime = currentTime;
+
+                GraphicsContext gc = canvas.getGraphicsContext2D();
+                gc.clearRect(0,0,1000,1000);
+                gc.setFill(Color.AQUA);
+                gc.fillOval(curX += 20, curY,40,40);
+            }
         }
     }
 }
