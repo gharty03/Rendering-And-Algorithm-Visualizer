428931bd8c9f69f1a4b032e3efecfa6cffd3c984
Cody Machine <codymachine93@gmail.com>
2022-01-09 23:48:50 -0800

    Complete Flood Fill
    Finished implementing flood fill.

--- src/main/java/com/example/renderingandalgorithmvisualizer/MenuButtonControllers/FloodFillController.java
+++ src/main/java/com/example/renderingandalgorithmvisualizer/MenuButtonControllers/FloodFillController.java
@@ -1,6 +1,7 @@
 package com.example.renderingandalgorithmvisualizer.MenuButtonControllers;
 
 import com.example.renderingandalgorithmvisualizer.Main;
+import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,7 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
@@ -18,30 +19,31 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.LinkedList;
-import java.util.Timer;
-import java.util.concurrent.TimeUnit;
 
 public class FloodFillController implements MenuControllerInterface
 {
     @FXML
     private Stage stage;
-    private Scene scene;
-    private Parent root;
-    private FXMLLoader fxmlLoader;
     @FXML
     private Canvas canvas;
     @FXML
-    private Button backButton;
-    @FXML
     private ColorPicker paintbrushColorPicker, floodFillColorPicker;
-    int brushSize, numCols, numRows;
-    Color[][] colorArray;
+    private int gridSize, numCols, numRows;
+    private Color[][] colorArray;
+    private boolean slowed;
+    private FFTimer ffTimer;
+    private LinkedList<FFCoordinate> queue;
+    private Color targetColor, replacementColor;
 
     public void initFloodFillWindow()
     {
-        brushSize = 5;
-        numCols = (int)canvas.getWidth() / brushSize;
-        numRows = (int)canvas.getHeight() / brushSize;
+        gridSize = 5;
+        numCols = (int)canvas.getWidth() / gridSize;
+        numRows = (int)canvas.getHeight() / gridSize;
+        slowed = false;
+        queue = null;
+        targetColor = null;
+        replacementColor = null;
         initCanvas();
     }
 
@@ -55,6 +57,7 @@ public class FloodFillController implements MenuControllerInterface
                 colorArray[a][b] = Color.web("#ffffff");
             }
         }
+
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.clearRect(0,0,1000,1000);
         gc.setFill(Color.web("#ffffff"));
@@ -62,10 +65,12 @@ public class FloodFillController implements MenuControllerInterface
     }
 
     @FXML
-    protected void onCanvasClicked(MouseEvent event) throws InterruptedException {
+    protected void onCanvasClicked(MouseEvent event)
+    {
         MouseButton clicked = event.getButton();
-        int cellPosX = (int)event.getX() / brushSize;
-        int cellPosY = (int)event.getY() / brushSize;
+        int cellPosX = (int)event.getX() / gridSize;
+        int cellPosY = (int)event.getY() / gridSize;
+
         if (clicked.name().equals("PRIMARY"))
         {
             colorArray[cellPosX][cellPosY] = paintbrushColorPicker.getValue();
@@ -73,50 +78,98 @@ public class FloodFillController implements MenuControllerInterface
         }
         else if (clicked.name().equals("SECONDARY"))
         {
-            floodFill(cellPosX, cellPosY, colorArray[cellPosX][cellPosY], floodFillColorPicker.getValue());
+            if (slowed)
+                floodFillSlowed(cellPosX, cellPosY, colorArray[cellPosX][cellPosY], floodFillColorPicker.getValue());
+            else
+                floodFill(cellPosX, cellPosY, colorArray[cellPosX][cellPosY], floodFillColorPicker.getValue());
         }
     }
 
-    private void floodFill(int xCol, int yRow, Color targetColor, Color replacementColor) throws InterruptedException {
+    private void floodFill(int xCol, int yRow, Color targetColor, Color replacementColor)
+    {
         if (targetColor.equals(replacementColor))
             return;
 
-        LinkedList<Coord> queue = new LinkedList<Coord>();
-        queue.add(new Coord(xCol,yRow));
+        this.targetColor = targetColor;
+        this.replacementColor = replacementColor;
+
+        queue = new LinkedList<>();
+        queue.add(new FFCoordinate(xCol, yRow));
 
         while (!queue.isEmpty())
         {
-            Coord n = queue.removeFirst();
+            FFCoordinate n = queue.removeFirst();
             if (colorArray[n.x][n.y].equals(targetColor))
             {
                 colorArray[n.x][n.y] = replacementColor;
                 paintSquareOnCanvas(n.x, n.y, replacementColor);
 
                 if (n.x > 0)
-                    queue.add(new Coord(n.x - 1, n.y));
+                    queue.add(new FFCoordinate(n.x - 1, n.y));
                 if (n.x < numCols - 1)
-                    queue.add(new Coord(n.x + 1, n.y));
+                    queue.add(new FFCoordinate(n.x + 1, n.y));
                 if (n.y > 0)
-                    queue.add(new Coord(n.x, n.y - 1));
+                    queue.add(new FFCoordinate(n.x, n.y - 1));
                 if (n.y < numRows - 1)
-                    queue.add(new Coord(n.x, n.y + 1));
+                    queue.add(new FFCoordinate(n.x, n.y + 1));
             }
         }
+    }
+
+    private void floodFillSlowed(int xCol, int yRow, Color targetColor, Color replacementColor)
+    {
+        if (targetColor.equals(replacementColor))
+            return;
+
+        queue = new LinkedList<>();
+        queue.add(new FFCoordinate(xCol, yRow));
+
+        this.targetColor = targetColor;
+        this.replacementColor = replacementColor;
+
+        if (ffTimer != null)
+            ffTimer.stop();
+        ffTimer = new FFTimer();
+        ffTimer.start();
+    }
+
+    private void floodFillSlowedProcessNeighborNodes()
+    {
+        if (!queue.isEmpty())
+        {
+            FFCoordinate n = queue.removeFirst();
+            if (colorArray[n.x][n.y].equals(targetColor))
+            {
+                colorArray[n.x][n.y] = replacementColor;
+                paintSquareOnCanvas(n.x, n.y, replacementColor);
+
+                if (n.x > 0)
+                    queue.add(new FFCoordinate(n.x - 1, n.y));
+                if (n.x < numCols - 1)
+                    queue.add(new FFCoordinate(n.x + 1, n.y));
+                if (n.y > 0)
+                    queue.add(new FFCoordinate(n.x, n.y - 1));
+                if (n.y < numRows - 1)
+                    queue.add(new FFCoordinate(n.x, n.y + 1));
+            }
+        }
+        else
+            ffTimer.stop();
     }
 
     private void paintSquareOnCanvas(int cellPosX, int cellPosY, Color color)
     {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(color);
-        gc.fillRect((cellPosX * brushSize),(cellPosY * brushSize), brushSize, brushSize);
+        gc.fillRect((cellPosX * gridSize),(cellPosY * gridSize), gridSize, gridSize);
     }
 
     @FXML
     protected void onCanvasMouseDragged(MouseEvent event)
     {
         MouseButton clicked = event.getButton();
-        int cellPosX = (int)event.getX() / brushSize;
-        int cellPosY = (int)event.getY() / brushSize;
+        int cellPosX = (int)event.getX() / gridSize;
+        int cellPosY = (int)event.getY() / gridSize;
         if (cellPosX < 0 || cellPosX >= numCols || cellPosY < 0 || cellPosY >= numRows)
             return;
         if (clicked.name().equals("PRIMARY"))
@@ -127,36 +180,55 @@ public class FloodFillController implements MenuControllerInterface
     }
 
     @FXML
-    protected void onResetCanvasButtonClicked(ActionEvent event)
+    protected void onResetCanvasButtonClicked()
     {
         initCanvas();
+        if (ffTimer != null)
+            ffTimer.stop();
+    }
+
+    @FXML
+    protected void onSlowedCheckBoxChanged(ActionEvent event)
+    {
+        slowed = ((CheckBox)event.getSource()).isSelected();
     }
 
     @Override
     public void OpenMenuWindow(ActionEvent event) throws IOException {
-        fxmlLoader = new FXMLLoader(Main.class.getResource("MenuWindow.fxml"));
-        root = fxmlLoader.load();
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MenuWindow.fxml"));
+        Parent root = fxmlLoader.load();
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setWidth(450);
         stage.setHeight(400);
         stage.setTitle("RAAV - Rendering and Algorithm Visualizer");
         stage.centerOnScreen();
         stage.show();
+        if (ffTimer != null)
+            ffTimer.stop();
     }
 
     @FXML
     public void QuitToDesktop(ActionEvent event)
     {
-        // todo
+        // TODO: Implement quit to desktop button.
     }
 
-    private class Coord
+    private class FFTimer extends AnimationTimer
+    {
+        @Override
+        public void handle(long currentTime)
+        {
+            floodFillSlowedProcessNeighborNodes();
+        }
+    }
+
+    private static class FFCoordinate
     {
         public int x, y;
 
-        Coord(int x, int y)
+        FFCoordinate(int x, int y)
         {
             this.x = x;
             this.y = y;
